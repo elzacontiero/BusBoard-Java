69fa7ce36050f6a5e8f304a66f2eb34ed9f5a173
Elza Contiero <contieroelza@gmail.com>
2022-09-20 23:19:35 +0100

    Start working on part 1

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -1,7 +1,28 @@
 package training.busboard;
 
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+import org.glassfish.jersey.jackson.JacksonFeature;
+
 public class Main {
-    public static void main(String args[]) {
-        // Your code here!
+
+    // Part 1 - Get bus arrivals for StopPoint 490008660N.
+
+   public static void part1() {
+        String tflEndpoint = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals";
+//        Client client = ClientBuilder.newClient();
+        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
+        String response = client.target(tflEndpoint)
+                .request(MediaType.APPLICATION_JSON)
+                .get(String.class);
+
+       System.out.println(response);
+
     }
-}	
+
+
+    public static void main(String args[]) {
+      part1();
+    }
+}
