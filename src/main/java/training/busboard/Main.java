83620397ddc60f3cd324eb79b6e9d79577023abd
Elza Contiero <contieroelza@gmail.com>
2022-09-22 17:21:55 +0100

    move BusArrival class to its own file.

--- src/main/java/training/busboard/Main.java
+++ src/main/java/training/busboard/Main.java
@@ -11,53 +11,6 @@ import java.util.Arrays;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class BusArrival {
-
-    public String id;
-    public String vehicleId;
-    public String stationName;
-    public String lineId;
-    public String lineName;
-    public String platformName;
-    public String direction;
-    public Integer timeToStation;
-    public String destinationName;
-    public String timeStamp;
-    public String currentLocation;
-    public String towards;
-    public String expectedArrival;
-
-    public static List<BusArrival> getListOfBusArrival(String stopPoint) {
-        String endpoint = "https://api.tfl.gov.uk/StopPoint/" + stopPoint + "/Arrivals";
-        Client client = ClientBuilder.newBuilder().register(JacksonFeature.class).build();
-        List<BusArrival> response = client.target(endpoint)
-                .request(MediaType.APPLICATION_JSON)
-                .get(new GenericType<List<BusArrival>>() {
-                });
-        return response;
-    }
-
-    @Override
-    public String toString() {
-        return "BusArrival{" +
-                "id='" + id + '\'' +
-                ", vehicleId='" + vehicleId + '\'' +
-                ", stationName='" + stationName + '\'' +
-                ", lineId='" + lineId + '\'' +
-                ", lineName='" + lineName + '\'' +
-                ", platformName='" + platformName + '\'' +
-                ", direction='" + direction + '\'' +
-                ", timeToStation=" + timeToStation +
-                ", destinationName='" + destinationName + '\'' +
-                ", timeStamp='" + timeStamp + '\'' +
-                ", currentLocation='" + currentLocation + '\'' +
-                ", towards='" + towards + '\'' +
-                ", expectedArrival='" + expectedArrival + '\'' +
-                '}';
-    }
-}
-
-@JsonIgnoreProperties(ignoreUnknown = true)
 class PostCodeResult {
     public String postcode;
     public Double longitude;
